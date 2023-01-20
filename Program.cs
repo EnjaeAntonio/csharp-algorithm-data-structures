dcd5d401dcce3feffc4cd1e5e1b005eaf9185912
Enjae Antonio <Enjaeantonio@outlook.com>
2023-01-20 12:12:58 -0600

    Finished question one, started the second question

--- Program.cs
+++ Program.cs
@@ -1,50 +1,4 @@
 ﻿/********* Question1: **********/
-
-//A vending machine has a set of coins to handle purchase operations.
-
-//When a customer buys a product from the machine and pays, the machine will return the remaining money from the coins in its internal storage.
-
-//Suppose our machine has the following coins:
-
-//$1, $2, $5, $10, $20
-
-
-//The machine tracks the quantity of each coin: for example, the quantity of $5 coins is 15, which means the machine currently has 15 pieces of $5 coins.
-
-
-//Write a program that represents the purchase operation.
-
-//Initialize a Dictionary to represent the coin denominations, and the quantities remaining of each coin denomination.
-//Initialize a second Dictionary that contains the possible items in the Vending Machine and their prices.You may hard -
-//code these values for demonstration purposes. For example, your machine may have the values: { { A: 3}, { B: 2}, { C: 1.50} }
-//to represent item A with the cost of $3, B with the cost of $2, and so on.
-//Your console program should prompt the user first for an amount of money, and then an item name.
-//If the item name is not found in the machine, the machine should prompt for a different input, but should store the money that the
-//user initially input; it should not ask for a new amount of money.
-//If the item costs more than what the user has input for money, it should also prompt the user for a selection that the user can afford.
-//If the user selects an item that is valid, and they have entered enough money to buy, the program should output
-//that it has vended the item by name,
-//and how many coins it returns for change. It should return the fewest number of coins possible.
-//If the machine does not have sufficient change to return to the user, it should not return any coins, and the entire operation should be canceled
-//and returned to the beginning.
-//If the user ever types "CANCEL", the entire transaction should end.
-    //Example:
-
-    //The user entered $25 and wants to buy a $2 item, so the machine should return the following:
-
-    //$20 : 1 piece 
-
-    //$2 : 1 piece
-
-    //$1 : 1 piece
-
-    //If the machine is out of a certain coin, it should not return it.
-
-    //The machine always returns the minimum amount of possible coins.
-    //For example, if the machine must return $7 in change, it should return one $5 coin and one $2 coin. If $5 coins are not available,
-    //it would return three $2 coins and one $1 coin. 
-
-// Coin Denominations
 Dictionary<int, int> coinDenominations = new Dictionary<int, int> {
         { 1, 15 },
         { 2, 10 },
@@ -63,14 +17,19 @@ Dictionary<string, double> itemInv = new Dictionary<string, double> {
 
 
 // Ask user for money and initialize user's change
-Console.WriteLine("Please enter the amount of money you have:");
+Console.WriteLine("ADD FUNDS: ");
 double userMoney = double.Parse(Console.ReadLine());
+while(userMoney <= 1)
+{
+    Console.WriteLine("FUNDS INSUFFICIENT");
+}
+
 double userChange = 0;
 while(userMoney > 0)
 {
     // Ask for item name
-    Console.WriteLine("Enter which item you would like");
-    Console.WriteLine("Type 'List' to view our options, 'Done' to finish your transaction, or 'Cancel' to cancel your purchase! :)");
+    Console.WriteLine("Enter which item you would like :)");
+    Console.WriteLine("Type 'List' to view our options, 'Done' to finish your transaction, or 'Cancel' to cancel your purchase!");
     string userItem = Console.ReadLine().ToUpper();
 
     if (userItem.ToUpper() == "CANCEL")
@@ -82,30 +41,45 @@ while(userMoney > 0)
     {
         foreach(KeyValuePair<string, double> item in itemInv)
         {
-            Console.WriteLine($"Item: {item.Key}, Cost: ${item.Value}");
+            Console.WriteLine($"* {item.Key}: ${item.Value} *");
+            Console.WriteLine();
+
         }
     }
     else if (userItem.ToUpper() == "DONE")
     {
         userChange = userMoney;
         Dictionary<int, int> userChangeCoins = new Dictionary<int, int>();
-            foreach (KeyValuePair<int, int> coin in coinDenominations)
+        List<KeyValuePair<int, int>> coinList = coinDenominations.ToList();
+        for(int i = 0; i < coinList.Count; i++)
+        {
+            for (int j = i + 1; j < coinList.Count; j++)
+            {
+                if (coinList[i].Key < coinList[j].Key)
+                {
+                    KeyValuePair<int, int> temp = coinList[i];
+                    coinList[i] = coinList[j];
+                    coinList[j] = temp;
+                }
+            }
+        }
+        foreach (KeyValuePair<int, int> coin in coinList)
             {
                 int coinCount = 0;
                 while (coin.Value > 0 && userChange >= coin.Key)
                 {
                     userChange -= coin.Key;
                     coinCount++;
-                    coinDenominations[coin.Key]--;
+                    coinDenominations[coin.Key] = coinDenominations[coin.Key] - coinCount;
                 }
                 if(coinCount > 0)
                 {
-                    userChangeCoins.Add(coin.Key, coin.Value);
+                    userChangeCoins.Add(coin.Key, coinCount);
                 }
             }
         foreach(KeyValuePair<int, int> coin in userChangeCoins)
         {
-            Console.WriteLine($"Returning ${coin.Value} : {coin.Key} piece(s)");
+            Console.WriteLine($"Returning ${coin.Key} : {coin.Value} piece(s)");
         }
         break;
     }
@@ -117,16 +91,18 @@ while(userMoney > 0)
     }
     else if (userMoney < itemInv[userItem])
     {
-        Console.WriteLine("Insufficient funds, please enter a different item!");
+        Console.WriteLine("* INSUFFICIENT FUNDS *, please enter a different item!");
+        Console.WriteLine();
         continue;
     }
     
     else
     {
-        Console.WriteLine($"Now vending {userItem}");
+        Console.WriteLine($"Now vending * {userItem} *");
         userChange = userMoney - itemInv[userItem];
         userMoney = userChange;
         Console.WriteLine($"Your remaining change is {userChange}");
+        Console.WriteLine();
     }
 }
 
